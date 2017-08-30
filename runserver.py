404a3ca6049e7571ca18397d9b9a6cb348372152
Andra Hendisutio <andra.hendisutio@dattabot.io>
2017-08-31 03:36:37 +0700

    First Commit

---
+++ runserver.py
@@ -0,0 +1,16 @@
+"""
+This script runs the Sparse application using a development server.
+"""
+
+from os import environ
+from Ahaditio import app
+
+#if __name__ == '__main__':
+#    HOST = environ.get('SERVER_HOST', 'localhost')
+#    try:
+#        PORT = int(environ.get('SERVER_PORT', '5555'))
+#    except ValueError:
+#        PORT = 5555
+#    app.run(HOST, PORT)
+
+app.run('0.0.0.0', 5008, debug=True, threaded= True)
